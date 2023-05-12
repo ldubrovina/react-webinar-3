26c3e4c3718a6aaa04fcdcc3b1f35a41f7c26e78
ldubrovina <dubrovinalv24@gmail.com>
2023-05-12 23:45:23 +0300

    fix: fixed the selection of the phrase according to the design

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createElement } from './utils.js';
+import {createElement} from './utils.js';
 import './styles.css';
 
 /**
@@ -7,39 +7,46 @@ import './styles.css';
  * @param store {Store} Хранилище состояния приложения
  * @returns {React.ReactElement}
  */
-function App({ store }) {
+function App({store}) {
 
-  const list = store.getState().list;
+    const list = store.getState().list;
 
-  return (
-    <div className='App'>
-      <div className='App-head'>
-        <h1>Приложение на чистом JS</h1>
-      </div>
-      <div className='App-controls'>
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
-      <div className='App-center'>
-        <div className='List'>{
-          list.map(item =>
-            <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
-                <div className='Item-actions'>
-                  <button onClick={() => store.deleteItem(item.code)}>
-                    Удалить
-                  </button>
-                </div>
-              </div>
-              {item.selectedCount > 0 && <div className='Item-selected-count'>Выделяли {item.selectedCount} раз(а)</div>}
+    return (
+        <div className='App'>
+            <div className='App-head'>
+                <h1>Приложение на чистом JS</h1>
             </div>
-          )}
+            <div className='App-controls'>
+                <button onClick={() => store.addItem()}>Добавить</button>
+            </div>
+            <div className='App-center'>
+                <div className='List'>{
+                    list.map(item =>
+                        <div key={item.code} className='List-item'>
+                            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
+                                 onClick={() => store.selectItem(item.code)}>
+                                <div className='Item-code'>{item.code}</div>
+                                <div className='Item-title'>
+                                    {item.title}
+                                    {
+                                        item.selectedCount > 0
+                                        &&
+                                        <span className='Item-selections'> | Выделяли {item.selectedCount} раз</span>
+                                    }
+                                </div>
+                                <div className='Item-actions'>
+                                    <button onClick={() => store.deleteItem(item.code)}>
+                                        Удалить
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
